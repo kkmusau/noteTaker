69e088362d25910bb7dfa1ff69e80c9a8737d870
kkmusau <kkmusau@gmail.com>
2022-06-22 20:50:50 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+// Require Dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+//Require routes file
+require('./routes/routes')(app);
+
+// Setup listener
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
